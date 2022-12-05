777b8171e89ffd6344c3cae655195d1ba920f44a
strangetamer43 <86204643+strangetamer43@users.noreply.github.com>
2022-12-05 06:12:43 +0530

    Update Questions.js

--- routes/Questions.js
+++ routes/Questions.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { createQuiz, editQUiz, getAllQuizOfUser, getQuizById, getAllQuizs, getQuizBySearchForUser, getQuizBySearch } from "../controllers/Challenges.js";
-import { submitResponse, getResponsesByUser, getResponse, getResponseByQuizId, submittingQuiz, getAllResponse, getResponseByUserName } from "../controllers/Response.js";
+import { createQuiz, editQUiz, getAllQuizOfUser, getQuizById, getAllQuizs, getQuizBySearchForUser, getQuizBySearch } from "../Controller/Challenges.js";
+import { submitResponse, getResponsesByUser, getResponse, getResponseByQuizId, submittingQuiz, getAllResponse, getResponseByUserName, createResponse } from "../Controller/Response.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
@@ -19,7 +19,9 @@ router.post("/getQuizBySearchForUser/:search", getQuizBySearchForUser);
 router.get("/getQuizBySearch/:search", getQuizBySearch)
 router.post("/submittingQuiz", submittingQuiz);
 router.get("/getResponseByUserName/:search", getResponseByUserName)
+router.get("/createResponse", createResponse)
 
 
 
-export default router;    
+
+export default router;  
