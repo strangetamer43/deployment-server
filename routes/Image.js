ac19d2d4e19a0f3a540c60ede46cda7b7b39f9dc
strangetamer43 <86204643+strangetamer43@users.noreply.github.com>
2022-12-05 06:12:07 +0530

    Update Image.js

--- routes/Image.js
+++ routes/Image.js
@@ -1,9 +1,11 @@
 import express from "express";
-import imageUpload from "../controllers/Image.js";
-import videoUpload from "../controllers/Video.js";
+import imageUpload from "../Controller/Image.js";
+import videoUpload from "../Controller/Video.js";
+import audioUpload from "../controller/audio.js";
 const router = express.Router();
 
 router.post("/uploadImage", imageUpload);
 router.post("/uploadVideo", videoUpload)
+router.post("/uploadAudio", audioUpload);
 
 export default router;
