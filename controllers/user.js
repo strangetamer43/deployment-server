9bb3df17041f6f9b1a5225df5bd72707c2c6825b
strangetamer43 <86204643+strangetamer43@users.noreply.github.com>
2022-12-11 18:28:19 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { v2 as cloudinary } from 'cloudinary';
-
+import fs from "fs";
 import dotenv from 'dotenv';
 
 import User from '../models/user.js';
@@ -22,17 +22,29 @@ export const signup = async (req, res) => {
         const pub = resultB.public_id;
         const existingUser = await User.findOne({ username });
 
-        if (existingUser) return res.status(400).json({ message: "User already exists!" });
-        if (password.length < 8) return res.status(400).json({ message: "Password is too short!" });
-        if (password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match!" });
+        if (existingUser) {
+            console.log("user exist")
+            return res.status(400).json({ message: "User already exists!" });
+        }
+        if (password.length < 8) {
+            console.log("password short")
+            return res.status(400).json({ message: "Password is too short!" });
+        }
+        if (password !== confirmPassword) {
+            console.log("password not match")
+            return res.status(400).json({ message: "Passwords don't match!" });
+        }
 
         const hashedPassword = await bcrypt.hash(password, 12);
-        if (phoneNumber.length !== 10) return res.status(400).json({ message: "Phone number is invalid!" });
+        if (phoneNumber.length !== 10) {
+            console.log("mobile number")
+            return res.status(400).json({ message: "Phone number is invalid!" });
+        }
         const result = await User.create({ username, password: hashedPassword, name, phoneNumber, emailId, imageUrl: img });
         try {
 
             const token = jwt.sign({ username: result.username, id: result._id }, 'test', { expiresIn: "2h" });
-
+            fs.unlinkSync(image.tempFilePath)
             res.status(200).json({ result, token });
         } catch (error) {
             console.log(error);
