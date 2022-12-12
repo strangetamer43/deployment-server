8af7348682993014ca6713f2f0ddfe7a57cd788b
strangetamer43 <86204643+strangetamer43@users.noreply.github.com>
2022-12-12 20:00:02 +0530

    Update questions.js

--- controllers/questions.js
+++ controllers/questions.js
@@ -165,7 +165,7 @@ export const getUserQuestions = async (req, res) => {
 };
 export const getSpecificUserQuestions = async (req, res) => {
     
-    const {creator} = req.body;
+    const creator = req.body.userId;
     
     try {
             
