6a1ba5fed831ae54a77e4b42db15cb4fe67eb273
strangetamer43 <86204643+strangetamer43@users.noreply.github.com>
2022-12-12 19:06:18 +0530

    Update Response.js

--- controllers/Response.js
+++ controllers/Response.js
@@ -1,6 +1,7 @@
 import responseModel from "../models/Response.js";
 import questionModel from "../models/Questions.js";
 import userModel from "../models/user.js";
+
 export const createResponse = async (req, res) => {
     try {
         var newResponse = new responseModel();
@@ -18,13 +19,16 @@ export const submitResponse = async (req, res) => {
     try {
         const response = req.body.response
         console.log(response)
+        const id = req.body.id;
+
+
         var data = {
             quizId: req.body.quizId,
             userId: req.body.userId,
             quizName: req.body.quizName,
             userName: req.body.userName,
             response,
-            videos: req.body.videos
+
         }
         console.log(data);
         questionModel.findById(data.quizId, async (err, result) => {
@@ -40,7 +44,6 @@ export const submitResponse = async (req, res) => {
                         if (result.questions[i].id === response[j].questionId) {
                             if (result.questions[i].questionType === "mcq") {
                                 var option = result.questions[i].options.find(o => o.option === response[j].option)
-                                console.log(option)
                                 correctAnswer += option.value;
                                 j += 1;
                             } else {
@@ -51,6 +54,7 @@ export const submitResponse = async (req, res) => {
                     }
 
                 }
+                console.log(correctAnswer)
                 let grade = ""
                 if (result.scoring === "normal") {
                     for (let i = 0; i < result.result.data.length; i++) {
@@ -73,19 +77,25 @@ export const submitResponse = async (req, res) => {
                         }
                     }
                 }
+                console.log(grade)
+                console.log(correctAnswer)
 
                 data["grade"] = grade;
                 data["correctAnswers"] = correctAnswer;
-                console.log(data)
-                var newResponse = new responseModel(data);
-                await newResponse.save().then((docs) => {
-                    userModel.updateOne(
-                        { _id: data.userId },
-                        { $push: { responses: data.quizId } })
-                        .then(() => {
-                            console.log("New quiz reponse is added")
-                        }).catch(error => console.log(error))
-                    res.status(203).json(docs);
+
+                responseModel.findByIdAndUpdate(id, data, { new: true }, (err, result) => {
+                    if (err) {
+                        res.status(403).json(err)
+                    } else {
+                        userModel.updateOne(
+                            { _id: data.userId },
+                            { $push: { responses: data.quizId } })
+                            .then(() => {
+                                console.log("New quiz reponse is added")
+                            }).catch(error => console.log(error))
+                    }
+                    console.log(result)
+                    res.status(203).json(result);
                 })
             }
         })
@@ -170,6 +180,7 @@ export const submittingQuiz = async (req, res) => {
         const userId = req.body.userId;
         const quizId = req.body.quizId;
 
+
         questionModel.findById(quizId, (err, qresult) => {
             if (err) {
                 res.status(403).json({ message: err.message })
@@ -177,6 +188,8 @@ export const submittingQuiz = async (req, res) => {
                 const attempts = qresult.attempts;
                 userModel.findById(userId, (err, uresult) => {
                     const responses = uresult.responses;
+                    console.log(responses[responses.length - 1])
+
                     if (err) {
                         res.status(403).json({ message: err.message })
                     } else {
@@ -207,6 +220,7 @@ export const submittingQuiz = async (req, res) => {
 }
 
 
+
 export const getResponseByUserName = async (req, res) => {
     try {
         const search = req.params.search;
@@ -234,3 +248,4 @@ export const getResponseByUserName = async (req, res) => {
 
 
 // get a response by a question id 
+
