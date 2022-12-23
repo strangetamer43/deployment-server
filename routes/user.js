f743db3a1395ea57b553df29065687800f1f1a61
strangetamer43 <86204643+strangetamer43@users.noreply.github.com>
2022-12-24 01:16:20 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,5 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.post("/createUser", createUser)
-router.patch("/getUser", getSpecificUser)
+router.patch("/", getSpecificUser)
 export default router;
