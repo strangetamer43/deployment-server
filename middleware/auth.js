e82f0395e22b1456fecd0d6526663edaadf59743
strangetamer43 <86204643+strangetamer43@users.noreply.github.com>
2022-12-29 11:40:56 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 const auth = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
@@ -6,7 +7,7 @@ const auth = async (req, res, next) => {
         const isCustomAuth = token.length < 500;
         let decodedData;
         if(token && isCustomAuth) {
-            decodedData = jwt.verify(token, 'test');
+            decodedData = jwt.verify(token, process.env.JWT_SECRET);
             req.userId = decodedData?.id;
         } else {
             decodedData = jwt.decode(token);
