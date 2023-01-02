756e48809feb819282a724070f799e66eca3b1fc
strangetamer43 <86204643+strangetamer43@users.noreply.github.com>
2023-01-02 13:53:57 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import auth from '../middleware/auth.js';
-import { signup, signin, createUser, getSpecificUser } from '../controllers/user.js';
+import { signup, signin, createUser, getSpecificUser, addFollower, removerFollower } from '../controllers/user.js';
 const router = express.Router();
 
 router.post('/signup', signup);
