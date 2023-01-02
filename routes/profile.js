b54209a0710df68bf51c0f0b503d5bef1d7960b0
strangetamer43 <86204643+strangetamer43@users.noreply.github.com>
2023-01-02 13:39:30 +0530

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,6 @@
 import express, { Router } from "express";
 import { getProfileByCreator } from "../controllers/posts.js";
-import { createProfile, updateProfile, getProfiles, getUserProfile, getSpecificUserProfile, getProfile } from "../controllers/profile.js";
+import { createProfile, updateProfile, getProfiles, getUserProfile, getSpecificUserProfile, getProfile, addExperience } from "../controllers/profile.js";
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
@@ -16,7 +16,7 @@ router.patch('/:id', auth, updateProfile);
 router.post('/userspecific', auth, getSpecificUserProfile);
 router.get('/:id', auth, getProfile);
 router.post('/getProfile', getProfileByCreator);
-
+router.patch("/addExperience/:id", auth, addExperience);
 
 
 export default router;
