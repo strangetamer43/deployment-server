1dc712207b9b3aca8207bccd537f9e19a8b7b276
strangetamer43 <86204643+strangetamer43@users.noreply.github.com>
2023-02-19 04:04:18 +0530

    Update Questions.js

--- routes/Questions.js
+++ routes/Questions.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { createQuiz, editQUiz, getAllQuizOfUser, getQuizById, getAllQuizs, getQuizBySearchForUser, getQuizBySearch } from "../controllers/Challenges.js";
-import { submitResponse, getResponsesByUser, getResponse, getResponseByQuizId, submittingQuiz, getAllResponse, getResponseByUserName, createResponse } from "../controllers/Response.js";
+import { createQuiz, editQUiz, getAllQuizOfUser, getQuizById, getAllQuizs, getQuizBySearchForUser, getQuizBySearch, getQuizsOfUserLazyLoading, getQuizsLazyLoading } from "../controllers/Challenges.js";
+import { submitResponse, getResponsesByUser, getResponse, getResponseByQuizId, submittingQuiz, getAllResponse, getResponseByUserName, createResponse, getResponseLazyLoading } from "../controllers/Response.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
@@ -18,9 +18,11 @@ router.get("/getAllResponse", getAllResponse) //get all the responses
 router.post("/getQuizBySearchForUser/:search", getQuizBySearchForUser);
 router.get("/getQuizBySearch/:search", getQuizBySearch)
 router.post("/submittingQuiz", submittingQuiz);
-router.get("/getResponseByUserName/:search", getResponseByUserName)
-router.get("/createResponse", createResponse)
-
+router.get("/getResponseByUserName/:search", getResponseByUserName);
+router.get("/createResponse", createResponse);
+router.post("/getResponseByPage", getResponseLazyLoading);
+router.post("/getQuizOfUserByPage", getQuizsOfUserLazyLoading);
+router.post("/getQuizByPage", getQuizsLazyLoading);
 
 
 
