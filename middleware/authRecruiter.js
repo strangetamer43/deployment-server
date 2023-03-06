2bcf2badde2bc0f18f1f9ed614f641a49dfc7123
strangetamer43 <86204643+strangetamer43@users.noreply.github.com>
2023-03-06 06:22:15 +0530

    Create authRecruiter.js

---
+++ middleware/authRecruiter.js
@@ -0,0 +1,19 @@
+import Recruiters from "../models/recruiter.js"
+const authRecruiter = async (req , res , next) => {
+    //get user info by id
+    const recruiter = await Recruiters.findOneById({
+        _id : req.Recruiter._id
+    })
+    if(recruiter.role ==0)
+    {
+        return res.status(400).json({msg : "Admin access denied"})
+    }
+    if(user.role == 1)
+    {
+        return res.status(400).json({msg : "Admin access approved"})
+
+    }
+    next() ;
+
+}
+export default authRecruiter;
