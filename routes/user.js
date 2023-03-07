5658cc1a670aaf2a2d1266248429531f8365ddab
strangetamer43 <86204643+strangetamer43@users.noreply.github.com>
2023-03-07 21:36:01 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import auth from '../middleware/auth.js';
-import { signup, signin, createUser, getSpecificUser, addFollower, removerFollower } from '../controllers/user.js';
+import { signup, signin, createUser, getUser, getSpecificUser, addFollower, removerFollower } from '../controllers/user.js';
 const router = express.Router();
 
 router.post('/signup', signup);
@@ -9,4 +9,5 @@ router.post("/createUser", createUser);
 router.patch("/", getSpecificUser);
 router.post("/follow", auth, addFollower);
 router.post("/unfollow", auth, removerFollower);
+router.get("/getUser/:id" ,getUser);
 export default router;
